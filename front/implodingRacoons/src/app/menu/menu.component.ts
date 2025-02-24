ae6347045cc69e677b604c2e2b4bb2d4a1012b91
GLa2s <129745244+Adricarmona@users.noreply.github.com>
2025-02-24 01:24:57 +0100

    paginacion login y registro

--- front/implodingRacoons/src/app/menu/menu.component.ts
+++ front/implodingRacoons/src/app/menu/menu.component.ts
@@ -3,11 +3,13 @@ import { LogueadoComponent } from "./navbar/logueado/logueado.component";
 import { SinLoguearComponent } from "./navbar/sin-loguear/sin-loguear.component";
 import { OpcionesMenuComponent } from "./opciones-menu/opciones-menu.component";
 import { FooterComponent } from "./footer/footer.component";
+import { RegistroComponent } from "./loguinRegistro/registro/registro.component";
+import { LoginComponent } from "./loguinRegistro/login/login.component";
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [LogueadoComponent, SinLoguearComponent, OpcionesMenuComponent, FooterComponent],
+  imports: [LogueadoComponent, SinLoguearComponent, OpcionesMenuComponent, FooterComponent, RegistroComponent, LoginComponent],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })

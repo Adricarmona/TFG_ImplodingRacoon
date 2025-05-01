50583c5ed89fde7a7105830783bb2c2d53a0274e
HectorDominguezG <145491924+HectorDominguezG@users.noreply.github.com>
2025-05-01 13:50:25 +0200

    creo que se arregla asi y login casi hecho

--- front/implodingRacoons/src/app/menu/loguinRegistro/login/login.component.ts
+++ front/implodingRacoons/src/app/menu/loguinRegistro/login/login.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthRequest } from '../../../models/auth-request';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  form: FormGroup;
+
+  async submit(){
+    const authData: AuthRequest = { 
+      email: this.form.get('email')?.value, 
+      password: this.form.get('password')?.value,
+      remember: this.remember
+    };
+  }
 
 }
--- front/implodingRacoons/src/app/service/auth.service.ts
+++ front/implodingRacoons/src/app/service/auth.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { ApiService } from './api.service';
+import { AuthRequest } from '../models/auth-request';
+import { AuthResponse } from '../models/auth-response';
+import { lastValueFrom, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private BASE_URL = `${environment.apiUrl}`;
+
+  constructor(
+    private http: HttpClient, 
+    private apiService: ApiService
+  ) { }
+
+  jwt: string = '';
+
+  async login(data: AuthRequest): Promise<AuthResponse | null> {
+    try {
+      const request: Observable<AuthResponse> = this.http.post<AuthResponse>(`${this.BASE_URL}Auth/login`, data);
+      const result: AuthResponse = await lastValueFrom(request);
+
+      this.apiService.jwt = result.token;
+
+      this.jwt = result.token;
+      if (data.remember) {
+        localStorage.setItem('token', this.jwt);
+      } else {
+        sessionStorage.setItem('token', this.jwt)
+      }
+
+      return result;
+    } catch (error)
+    {
+      console.error('Error logging in');
+      return null;
+    }
+  } 
 }

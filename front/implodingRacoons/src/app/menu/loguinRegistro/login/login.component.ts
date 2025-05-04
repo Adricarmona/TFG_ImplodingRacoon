69d53e0d42790aa85dfe99959a7912e7d5f658c7
HectorDominguezG <145491924+HectorDominguezG@users.noreply.github.com>
2025-05-04 16:57:27 +0200

    creo que el login esta pero no compila😭

--- front/implodingRacoons/src/app/menu/loguinRegistro/login/login.component.ts
+++ front/implodingRacoons/src/app/menu/loguinRegistro/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthRequest } from '../../../models/auth-request';
+import { AuthService } from '../../../service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -13,12 +14,27 @@ export class LoginComponent {
 
   form: FormGroup;
 
+  constructor(
+    private authService: AuthService, 
+    public fb: FormBuilder
+  )
+  {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  remember: boolean = false;
+
   async submit(){
     const authData: AuthRequest = { 
       email: this.form.get('email')?.value, 
       password: this.form.get('password')?.value,
       remember: this.remember
     };
+
+    this.authService.login(authData);
   }
 
 }

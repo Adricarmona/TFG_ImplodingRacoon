79f886a3cd16cfc03a308213d97282f1e5324a90
GLa2s <129745244+Adricarmona@users.noreply.github.com>
2025-05-19 22:58:20 +0200

    cosillas de la web

--- front/implodingRacoons/src/app/menu/loguinRegistro/login/login.component.ts
+++ front/implodingRacoons/src/app/menu/loguinRegistro/login/login.component.ts
@@ -1,40 +1,17 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { AuthRequest } from '../../../models/auth-request';
-import { AuthService } from '../../../service/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
-  form: FormGroup;
 
-  constructor(
-    private authService: AuthService, 
-    public fb: FormBuilder
-  )
-  {
-    this.form = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]]
-    });
-  }
+  constructor(){}
 
   remember: boolean = false;
-
-  async submit(){
-    const authData: AuthRequest = { 
-      email: this.form.get('email')?.value, 
-      password: this.form.get('password')?.value,
-      remember: this.remember
-    };
-
-    this.authService.login(authData);
-  }
 
 }
--- front/implodingRacoons/src/environments/environment.ts
+++ front/implodingRacoons/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    imageUrl: '',
     apiUrl: 'https://localhost:7089/',
     socketUrl: 'https://localhost:7089/socket',
 

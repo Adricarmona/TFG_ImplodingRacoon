fcc0c2cce9b00abcb0c39a86b3050e0da1b95013
GLa2s <129745244+Adricarmona@users.noreply.github.com>
2025-05-25 01:19:09 +0200

    cosillas

--- front/implodingRacoons/src/app/pages/wiki/wiki.component.ts
+++ front/implodingRacoons/src/app/pages/wiki/wiki.component.ts
@@ -4,24 +4,45 @@ import { NavbarComponent } from "./navbar/navbar.component";
 import { WikiService } from '../../service/wiki.service';
 import { PublicacionTarjeta } from '../../models/publicacion-tarjeta';
 import { DatePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-wiki',
   standalone: true,
-  imports: [RouterModule, NavbarComponent, DatePipe],
+  imports: [RouterModule, NavbarComponent, DatePipe, FormsModule],
   templateUrl: './wiki.component.html',
   styleUrl: './wiki.component.css'
 })
 export class WikiComponent implements OnInit {
   PostBuscados: PublicacionTarjeta[] = [];
 
-  constructor(private wikiService: WikiService){}
+  buscador: string = "";
+
+  constructor(
+    private wikiService: WikiService
+  ){}
 
   async ngOnInit() {
     try {
-      this.PostBuscados = await this.wikiService.cogerPostPorNombre("s");
+      this.PostBuscados = await this.wikiService.cogerPosts();
     } catch (error) {
       console.error('Error cargando posts:', error);
     }
   }
+
+  async busqueda() {
+    try {
+      this.PostBuscados = []
+      if (this.buscador.trim().length < 1) {
+        this.PostBuscados = await this.wikiService.cogerPosts();
+      } else {
+        this.PostBuscados = await this.wikiService.cogerPostPorNombre(this.buscador);
+      }
+
+
+    } catch (error) {
+      console.error('Error cargando posts:', error);
+    }
+  }
+
 }

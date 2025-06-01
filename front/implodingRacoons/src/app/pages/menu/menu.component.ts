10d423c36205a5d3eefba9b8794c7eca4a0a658a
GLa2s <129745244+Adricarmona@users.noreply.github.com>
2025-06-01 21:10:44 +0200

    alerta y cosilas

--- front/implodingRacoons/src/app/pages/menu/menu.component.ts
+++ front/implodingRacoons/src/app/pages/menu/menu.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OpcionesMenuComponent } from "./opciones-menu/opciones-menu.component";
 import { FooterComponent } from "./componentes/footer/footer.component";
 import { RegistroComponent } from "./loguinRegistro/registro/registro.component";
 import { LoginComponent } from "./loguinRegistro/login/login.component";
-import { StateService } from '../../service/state.service';
 import { AuthService } from '../../service/auth.service';
 import { LogueadoComponent } from './componentes/navbar/logueado/logueado.component';
 import { SinLoguearComponent } from './componentes/navbar/sin-loguear/sin-loguear.component';
@@ -13,7 +11,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [LogueadoComponent, SinLoguearComponent, OpcionesMenuComponent, FooterComponent, RegistroComponent, LoginComponent, CommonModule, RouterModule],
+  imports: [LogueadoComponent, SinLoguearComponent, FooterComponent, RegistroComponent, LoginComponent, CommonModule, RouterModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
@@ -28,5 +26,9 @@ export class MenuComponent {
     if(this.authService.existeUsuario()) {
       this.logueado = true
     }
+  }
+
+  alertaWiki() {
+    alert("Inicia session para entrar a la wiki")
   }
 }
